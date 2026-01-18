16403491f073ca89eca97d0f37f584bbac9055bf
DuyKhuong <khuongtran227@gmail.com>
2026-01-18 23:53:09 +0700

    update deploy koeyb

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 set -eu
 
-echo "Start blog :8080"
-APP_PORT="${APP_PORT:-8080}" /app/blog &
+export APP_PORT="${APP_PORT:-8080}"
+export PRODUCT_PORT="${PRODUCT_PORT:-8081}"
+
+echo "Start blog :$APP_PORT"
+APP_PORT="$APP_PORT" /app/blog &
 
 : "${DATABASE_URL:?DATABASE_URL is required}"
-echo "Start product :8081"
-PRODUCT_PORT="${PRODUCT_PORT:-8081}" node /app/dist/main.js &
+
+echo "Start product :$PRODUCT_PORT"
+DATABASE_URL="$DATABASE_URL" PRODUCT_PORT="$PRODUCT_PORT" node /app/dist/main.js &
 
 echo "Start caddy :${PORT}"
 exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
